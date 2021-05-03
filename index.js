0917c5b0c34f5df6b35938d80371134be29919d7
Ailiniyazi <a.maimaiti@fkc-online.com>
2021-05-03 15:38:19 +0200

    finish show result view

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const path = require('path');
 const app = express();
 
 const port = 3000;
-
-var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -18,7 +15,6 @@ app.get('/', (req, res) => {
 })
 
 app.post('/word', urlencodedParser, (req, res) => {
-    console.log(`post` + req.body.word);
     res.redirect('/word/' + req.body.word);
 })
 
@@ -27,11 +23,8 @@ app.get('/word/:word', (req, res) => {
     fetch(apiURL)
         .then(res => res.json())
         .then(json => {
-            console.log(JSON.stringify(json[0]));
             let searchedWord = json[0];
             res.render('word', { data: searchedWord });
-        }).catch(err => { 
-            res.send('No Word' + err.message + ' ' + req.params.word);
         });
 })
 
